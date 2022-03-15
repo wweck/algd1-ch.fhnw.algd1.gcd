386aa9af084257fee50a1d09448eb0797547ac03
Wolfgang Weck <wolfgang.weck@surface7>
2022-03-15 20:49:33 +0100

    removes unnecessary imports

--- src/test/java/ch/fhnw/algd1/gcd/GcdIterTest.java
+++ src/test/java/ch/fhnw/algd1/gcd/GcdIterTest.java
@@ -1,6 +1,5 @@
 package ch.fhnw.algd1.gcd;
 
-import ch.fhnw.algd1.gcd.GcdIter;
 import ch.fhnw.algd1.gcd.framework.IGcd;
 
 public class GcdIterTest extends AbstractGcdTest {
--- src/test/java/ch/fhnw/algd1/gcd/GcdRecTest.java
+++ src/test/java/ch/fhnw/algd1/gcd/GcdRecTest.java
@@ -1,6 +1,5 @@
 package ch.fhnw.algd1.gcd;
 
-import ch.fhnw.algd1.gcd.GcdRec;
 import ch.fhnw.algd1.gcd.framework.IGcd;
 
 public class GcdRecTest extends AbstractGcdTest{
